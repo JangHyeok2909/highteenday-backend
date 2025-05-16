70611173215eb117dc576c5634d3b64bef9a8705
bro-sn <sinyada3@gmail.com>
2025-05-16 11:44:18 +0900

    고등학교 기준으로 (학교정보, 급식정보) api

--- src/main/java/com/example/highteenday_backend/domain/schools/SchoolRepository.java
+++ src/main/java/com/example/highteenday_backend/domain/schools/SchoolRepository.java
@@ -1,11 +1,14 @@
 package com.example.highteenday_backend.domain.schools;
 
 
+import com.example.highteenday_backend.enums.SchoolCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SchoolRepository extends JpaRepository<School,Long> {
+    List<School> findByCategory(SchoolCategory category);
     Optional<School> findByCode(Integer code);
     boolean existsByCode(Integer code);  // 중복 학교 코드 확인
 }
