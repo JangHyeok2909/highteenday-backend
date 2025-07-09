ac5388420df60091ac83a1b566dd8066b238f480
GUBBIB <moon.dragon250@gmail.com>
2025-07-10 01:57:01 +0900

    log 코드 추가

--- src/main/java/com/example/highteenday_backend/security/OAuth2SuccessHandler.java
+++ src/main/java/com/example/highteenday_backend/security/OAuth2SuccessHandler.java
@@ -31,6 +31,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
         log.info("successhandler 진입");
         OAuth2User oaAuth2User = (OAuth2User) authentication.getPrincipal();
+        log.info("네이버 OAuth 전체 attributes: {}", oaAuth2User.getAttributes());
+
         String email = oaAuth2User.getAttribute("parsed_email");
 
         boolean isGuest = authentication.getAuthorities().stream()
