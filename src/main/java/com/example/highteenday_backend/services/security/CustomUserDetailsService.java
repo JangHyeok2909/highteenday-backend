fbede0ec0d41470b00a8443742a427b0e7c7fdd8
GUBBIB <moon.dragon250@gmail.com>
2025-07-14 01:57:48 +0900

    form login - 코드 수정

--- src/main/java/com/example/highteenday_backend/services/security/CustomUserDetailsService.java
+++ src/main/java/com/example/highteenday_backend/services/security/CustomUserDetailsService.java
@@ -1,7 +1,7 @@
 package com.example.highteenday_backend.services.security;
 
 import com.example.highteenday_backend.domain.users.UserRepository;
-import com.example.highteenday_backend.security.CustomUserDetails;
+import com.example.highteenday_backend.security.CustomUserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-                .map(CustomUserDetails::new)
+                .map(CustomUserPrincipal::new)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
     }
 }
