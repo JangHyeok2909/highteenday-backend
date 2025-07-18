f09c541af0b4d6bbdab16bda925f98f26a32185e
GUBBIB <moon.dragon250@gmail.com>
2025-07-18 15:53:26 +0900

    register - Service단으로 변경

--- src/main/java/com/example/highteenday_backend/controllers/UserController.java
+++ src/main/java/com/example/highteenday_backend/controllers/UserController.java
@@ -9,6 +9,7 @@ import com.example.highteenday_backend.security.CustomException;
 import com.example.highteenday_backend.security.CustomUserPrincipal;
 import com.example.highteenday_backend.security.TokenException;
 import com.example.highteenday_backend.security.TokenProvider;
+import com.example.highteenday_backend.services.domain.UserService;
 import com.example.highteenday_backend.services.security.JwtCookieService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -38,6 +39,7 @@ import java.util.Optional;
 @Slf4j
 public class UserController {
     private final UserRepository userRepository;
+    private final UserService userService;
     private final TokenProvider tokenProvider;
     private final PasswordEncoder passwordEncoder;
     private final JwtCookieService jwtCookieService;
@@ -97,57 +99,14 @@ public class UserController {
     }
 
 
-    @Transactional
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(
 //            @AuthenticationPrincipal OAuth2User oAuth2User, // RegisterUserDto 에 있지만 변조에 대비해서 원본 데이터를 사용/ 하려고 했는데 로그인 전에는 이거 사용 불가
             @RequestBody  RegisterUserDto registerUserDto,
             HttpServletResponse response
             ){
-        String email = registerUserDto.email();
-        if (userRepository.findByEmail(email).isPresent()) {
-            throw new CustomException(ErrorCode.ALREADY_EXISTS_USER);
-        }
-//        Optional<User> userOpt = userRepository.findByEmail(email);
-//        if(!userOpt.isEmpty()){
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이미 회원가입 한 유저");
-//        }
 
-        System.out.println("✅ registerUserDto.nickname = " + registerUserDto.nickname());
-
-        User user = new User();
-        user.setName(registerUserDto.name());
-        user.setNickname(registerUserDto.nickname());
-        user.setProvider(Provider.valueOf(registerUserDto.provider().toUpperCase()));
-        user.setEmail(registerUserDto.email());
-
-        String hashedPassword = passwordEncoder.encode(registerUserDto.password());
-        user.setHashedPassword(hashedPassword);
-
-        Map<String, Object> attributes = new HashMap<>();
-
-        System.out.println("✅ 현재 제공자 : " + registerUserDto.provider());
-
-        if (registerUserDto.provider().equalsIgnoreCase("DEFAULT")) {
-            attributes = Collections.emptyMap();
-        } else {
-            attributes.put("email", email);
-            attributes.put("name", registerUserDto.name());
-            attributes.put("provider", registerUserDto.provider());
-        }
-
-        // 저장 후 토큰 발급하기 위한 처리 코드
-        User savedUser = userRepository.saveAndFlush(user);
-
-        CustomUserPrincipal userDetails = new CustomUserPrincipal(savedUser, attributes, "ROLE_USER");
-
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-                userDetails,
-                null,
-                userDetails.getAuthorities()
-        );
-
-        jwtCookieService.setJwtCookie(authentication,response);
+        userService.register(registerUserDto, response);
 
         return ResponseEntity.ok("회원가입 성공");
     }
@@ -166,7 +125,6 @@ public class UserController {
 //            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "존재하지 않는 사용자입니다."));
 //        }
 //        User user = userOptional.get();
-
 
         if (!passwordEncoder.matches(dto.password(), user.getHashedPassword())) {
             throw new CustomException(ErrorCode.INVALID_PASSWORD);
