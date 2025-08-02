9d2866291de2f35635e5623dc87f6474ebebcd84
Jang Hyeok <wkdgur456789@gmail.com)>
2025-08-02 15:42:01 +0900

    add search school controller

--- src/main/java/com/example/highteenday_backend/controllers/SchoolController.java
+++ src/main/java/com/example/highteenday_backend/controllers/SchoolController.java
@@ -3,6 +3,7 @@ package com.example.highteenday_backend.controllers;
 import com.example.highteenday_backend.domain.schools.School;
 import com.example.highteenday_backend.services.domain.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/schools")
+@RequestMapping("/api/schools")
 public class SchoolController {
 
     @Autowired
     private SchoolService schoolService;
 
     @GetMapping("/search")
-    public List<School> searchSchools(@RequestParam("name") String name) {
-        return schoolService.searchSchoolName(name);
+    public ResponseEntity<List<School>> searchSchools(@RequestParam("name") String name) {
+        name = name.replaceAll(" ", "");
+        List<School> schools = schoolService.searchSchoolName(name);
+        return ResponseEntity.ok(schools);
     }
 }
