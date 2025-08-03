6bb95c2584bd19ccc1bcdbab19bb6790fd3bb953
GUBBIB <moon.dragon250@gmail.com>
2025-08-03 15:54:58 +0900

    친구 신청 ë, 응답 됩니당 / profileImgModify 가능

---
+++ src/main/java/com/example/highteenday_backend/services/domain/UserMediaService.java
@@ -0,0 +1,47 @@
+package com.example.highteenday_backend.services.domain;
+
+import com.example.highteenday_backend.domain.medias.Media;
+import com.example.highteenday_backend.domain.users.User;
+import com.example.highteenday_backend.domain.users.UserRepository;
+import com.example.highteenday_backend.dtos.FileInfo;
+import com.example.highteenday_backend.enums.MediaOwner;
+import com.example.highteenday_backend.services.global.S3Service;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserMediaService {
+
+    private final S3Service s3Service;
+    private final MediaService mediaService;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void updateProfileImage(User user, String newImage){
+
+        String currentUrl = user.getProfileUrl();
+
+        if (currentUrl != null && !currentUrl.isEmpty()) {
+            String pastKey = s3Service.getKeyByUrl(currentUrl);
+            s3Service.delete(pastKey);
+            mediaService.deleteMediaByUrl(currentUrl);
+        }
+
+        if (newImage == null || newImage.isEmpty()) {
+            user.setProfileUrl(null); // 기본 프사 변경인데 일단 null 넣음
+            return;
+        }
+
+        String finalUrl = s3Service.copyToFinalLocation(newImage, user.getId(), MediaOwner.PROFILE);
+        String newKey = s3Service.getKeyByUrl(finalUrl);
+        FileInfo newFileInfo = s3Service.getFileInfo(newKey);
+        Media media = mediaService.createMedia(newFileInfo);
+        media.setProfileOwner(user);
+
+        user.setProfileUrl(finalUrl);
+        s3Service.deleteUserTmp(user.getId());
+    }
+
+}
