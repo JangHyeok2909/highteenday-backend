58b39328c1b8934223d7fd554376e55ac566048d
GUBBIB <moon.dragon250@gmail.com>
2025-08-26 14:03:23 +0900

    build 안되는거 해결

--- src/main/java/com/example/highteenday_backend/dtos/Verification/VerifyCodeDto.java
+++ src/main/java/com/example/highteenday_backend/dtos/Verification/VerifyCodeDto.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public record VerifyCodeDto(
     String phoneNum,
     String verifyCode
