d29b0d43cbdf87406837cc902d0d9cef204056ce
Jang Hyeok <wkdgur456789@gmail.com)>
2026-04-26 13:33:31 +0900

    fix: handle 404 error caused by Google default image during upload

--- src/main/java/com/example/highteenday_backend/services/domain/UserMediaService.java
+++ src/main/java/com/example/highteenday_backend/services/domain/UserMediaService.java
@@ -4,10 +4,13 @@ import com.example.highteenday_backend.domain.medias.Media;
 import com.example.highteenday_backend.domain.users.User;
 import com.example.highteenday_backend.dtos.FileInfo;
 import com.example.highteenday_backend.enums.MediaOwner;
+import com.example.highteenday_backend.exceptions.ResourceNotFoundException;
 import com.example.highteenday_backend.services.global.S3Service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,15 +23,17 @@ public class UserMediaService {
     public void updateProfileImage(User user, String newImage){
 
         String currentUrl = user.getProfileUrl();
-        //기존 이미지 존재하면 삭제
+        //기존 이미지 존재하면 삭제. 구글 기본 이미지의 경우엔 무시
         if (currentUrl != null && !currentUrl.isEmpty()) {
-            String pastKey = s3Service.getKeyByUrl(currentUrl);
-            s3Service.delete(pastKey);
-            mediaService.deleteMediaByUrl(currentUrl);
+            try {
+                String pastKey = s3Service.getKeyByUrl(currentUrl);
+                s3Service.delete(pastKey);
+                mediaService.deleteMediaByUrl(currentUrl);
+            } catch (ResourceNotFoundException e){}
         }
-
+        // 변경할 프로필 없으면 그냥 기본프로필 사용
         if (newImage == null || newImage.isEmpty()) {
-            user.setProfileUrl(null); // 기본 프사 변경인데 일단 null 넣음
+            user.setProfileUrl(null);
             return;
         }
         //이미지 업데이트
